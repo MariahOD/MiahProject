c95400a831a9cf7e4c0a05257f8e82414870f698
unknown <mariah.odoherty23@mail.dcu.ie>
2018-12-05 04:27:55 +0000

    g

--- Scripts/ChangeBanner.js
+++ Scripts/ChangeBanner.js
@@ -2,7 +2,7 @@ function getImage()
 {
 
     var images = [
-        "Images/dagger.jpg", "Images/witch.jpg"
+        "Images/dagger.jpg", "Images/witch.jpg", "Images/Hat2.jpg"
     ];
 
     var index = Math.floor(Math.random() * images.length);
