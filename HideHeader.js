766006dcc0d49edcd46cc28a26f828e6b44dee1c
unknown <mariah.odoherty23@mail.dcu.ie>
2018-12-05 18:24:49 +0000

    g

---
+++ HideHeader.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+var a = document.getElementsByClassName("pp");          // array           
+for (var i = 0; i < a.length; i++)
+  // if i is current page then show a[i] 
+  // else hide a[i]
