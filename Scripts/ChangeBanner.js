24bf324b0d200ed56f7504095206a0ef3c687e3a
unknown <mariah.odoherty23@mail.dcu.ie>
2018-12-05 01:12:19 +0000

    g

--- Scripts/ChangeBanner.js
+++ Scripts/ChangeBanner.js
@@ -2,7 +2,7 @@ function getImage()
 {
 
     var images = [
-        "Images/dagger.jpg", "Images/Hat.jpg"
+        "Images/dagger.jpg", "Images/witch.jpg"
     ];
 
     var index = Math.floor(Math.random() * images.length);
